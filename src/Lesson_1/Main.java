4bd4f26cb892b2ca77e7680c28c455f0d52e9a45
SuperArtemka <superak@bk.ru>
2023-09-24 15:03:05 +0300

    Приветствие по времени суток

---
+++ src/Lesson_1/Main.java
@@ -0,0 +1,31 @@
+package Lesson_1.Task_1;
+
+import java.time.LocalTime;
+import java.util.Scanner;
+
+public class Main {
+    //  В консоли запросить имя пользователя. В зависимости от текущего времени, вывести приветствие вида
+//  "Доброе утро, <Имя>!", если время от 05:00 до 11:59
+//  "Добрый день, <Имя>!", если время от 12:00 до 17:59;
+//  "Добрый вечер, <Имя>!", если время от 18:00 до 22:59;
+//  "Доброй ночи, <Имя>!", если время от 23:00 до 4:59
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите ваше имя: ");
+        String name = scanner.nextLine();
+        LocalTime time = LocalTime.now();
+        int hour = time.getHour();
+        if (hour > 5 && hour < 12){
+            System.out.println("Доброе утро, " + name + "!");
+        }
+        else if (hour > 12 && hour < 18){
+            System.out.println("Добрый день, " + name + "!");
+        }
+        else if (hour > 18 && hour < 23){
+            System.out.println("Добрый вечер, " + name + "!");
+        }
+        else {
+            System.out.println("Доброй ночи, " + name + "!");
+        }
+    }
+}
