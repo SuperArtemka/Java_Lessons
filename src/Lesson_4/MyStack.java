948020243d5e519375bb71d7c23d9b50cb6a6de8
SuperArtemka <superak@bk.ru>
2023-10-14 00:38:08 +0300

    Lesson_4

---
+++ src/Lesson_4/MyStack.java
@@ -0,0 +1,32 @@
+package Lesson_4;
+
+
+public class MyStack {
+    private String[] arr = new String[10];
+    private int size = 0;
+
+    public int size(){
+        return size;
+    }
+
+    public boolean empty(){
+        return size == 0;
+    }
+
+    public void push(String s){
+        if (size >= arr.length){
+            String[] arr2 = new String[arr.length * 2];
+            System.arraycopy(arr, 0, arr2, 0, arr.length);
+            arr = arr2;
+        }
+        arr[size++] = s;
+    }
+
+    public String peek(){
+        return arr[size-1];
+    }
+
+    public String pop(){
+        return arr[--size];
+    }
+}
